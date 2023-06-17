4f24f92fe6207cac2408415293ca83bf59828208
Avinash <sc2459@njit.edu>
2023-06-16 21:25:59 -0400

    my first commit

---
+++ src/components/h1tag/H1Tag.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./H1Tag.css";
+
+const H1Tag = () => {
+  return (
+    <div className="color">
+      <h1>you can click the below button</h1>
+    </div>
+  );
+};
+
+export default H1Tag;
